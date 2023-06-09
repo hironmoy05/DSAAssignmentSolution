681aa7a25868afa74b9e057ac1cc02df06128d3d
gauravzone <gauravdhar35@gmail.com>
2023-06-09 12:06:17 +0530

    Added subtract product and sum leetcode questions

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -161,11 +161,31 @@ public class Conditionals {
         return fibonacciSeries2(n - 1) + fibonacciSeries2(n - 2);
     }
 
+    // Subtract the Product and Sum of Digits of an Integer
+    /*
+        Input: n = 234
+        Output: 15
+        Explanation:
+        Product of digits = 2 * 3 * 4 = 24
+        Sum of digits = 2 + 3 + 4 = 9
+        Result = 24 - 9 = 15
+    */
+    public static int productAndSum(int n) {
+        int p = 1;
+        int s = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            p *= lastDigit;
+            s += lastDigit;
+
+            n /= 10;
+        }
+
+        return p - s;
+    }
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++)
-            System.out.print(fibonacciSeries2(i) + " ");
-
+        System.out.println(productAndSum(234));
     }
 }
